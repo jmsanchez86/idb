46897ca7ae29e41eeac0950a91321dfb6abd5a07
Noel Benzinger <noel.benzinger@utexas.edu>
2017-03-16 17:08:29 -0500

    Turned on echoes.

--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///:memory:'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 class NutrientCategory(enum.Enum):
