8dc9a1ee0899bba59e97bbfbd243fa3e129ab8f2
Scott Munro <scottmunro@utexas.edu>
2017-03-21 17:34:49 -0500

    updating the client.min.js file being served statically to users

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = vennfridge.create_app(config)
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8090, debug=True)
