cabdf5f7c6cfb129e46286003c0c2f2948758110
Scott Munro <scottnmunro@gmail.com>
2017-03-23 20:12:50 -0500

    app var name no longer app so it needed changing to deploy

--- main.py
+++ main.py
@@ -4,12 +4,5 @@
 import app
 import config
 
-
-# This is only used when running locally. When running live, gunicorn runs
-# the application.
-def main():
-    flask_app = app.create_app(config)
-    flask_app.run(host='127.0.0.1', port=8090, debug=True)
-
-if __name__ == '__main__':
-    main()
+FLASK_APP = app.create_app(config)
+FLASK_APP.run(host='127.0.0.1', port=8090, debug=True)
