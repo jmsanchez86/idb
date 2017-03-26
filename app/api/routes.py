2d8b7007ba71c1adfcbdbfd0f8ab56618eec4d80
Scott Munro <scottnmunro@gmail.com>
2017-03-26 02:40:23 -0500

    browse endpoints respond to pagesize and page number
    this is useful for pagination and continues
    
    Some articles site this as a harmful pattern, but since our
    data source isn't changing I think it'll work just fine
    
    http://blog.vermorel.com/journal/2015/5/8/nearly-all-web-apis-get-paging-wrong.html

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,37 +1,64 @@
 # pylint: disable=missing-docstring
 
+from functools import wraps
 import flask
 from app.api import food_data
 
 API_BP = flask.Blueprint('api', __name__)
 
+################
+# Browse Views #
+################
+
+def continuation_route(route_fn):
+    from flask import request
+    @wraps(route_fn)
+    def wrapped_route_function(*args, **kwargs):
+        page = request.args.get("page") if "page" in request.args else 0
+        psize = request.args.get("page_size") if "page_size" in request.args else 0
+        return route_fn(page, psize, *args, **kwargs)
+    return wrapped_route_function
+
+
 @API_BP.route('/ingredients')
-def get_all_ingredients():
-    return '{"result": "all ingredients"}'
+@continuation_route
+def get_all_ingredients(page, page_size):
+    return flask.json.jsonify({"page": page, "page_size": page_size,
+                               "type": "ingredients"})
+@API_BP.route('/recipes')
+@continuation_route
+def get_all_recipes(page, page_size):
+    return flask.json.jsonify({"page": page, "page_size": page_size,
+                               "type": "recipes"})
+
+@API_BP.route('/grocery_items/')
+@continuation_route
+def get_all_grocery_items(page, page_size):
+    return flask.json.jsonify({"page": page, "page_size": page_size,
+                               "type": "grocery items"})
+
+@API_BP.route('/tags')
+@continuation_route
+def get_all_tags(page, page_size):
+    return flask.json.jsonify({"page": page, "page_size": page_size,
+                               "type": "tags"})
+
+
+################
+# Detail Views #
+################
 
 @API_BP.route('/ingredients/<int:ingredient_id>')
 def get_ingredient(ingredient_id: int):
     return flask.json.jsonify(food_data.ingredients[ingredient_id])
 
-@API_BP.route('/recipes')
-def get_all_recipes():
-    return '{"result": "all recipes"}'
-
 @API_BP.route('/recipes/<int:recipe_id>')
 def get_recipe(recipe_id: int):
     return flask.json.jsonify(food_data.recipes[recipe_id])
 
-@API_BP.route('/grocery_items/')
-def get_all_grocery_items():
-    return '{"result": "all grocery items"}'
-
 @API_BP.route('/grocery_items/<int:grocery_item_id>')
 def get_grocery_items(grocery_item_id: int):
     return flask.json.jsonify(food_data.grocery_items[grocery_item_id])
-
-@API_BP.route('/tags')
-def get_all_tags():
-    return '{"result": "all tags"}'
 
 @API_BP.route('/tags/<int:tag_id>')
 def get_tag(tag_id: int):
