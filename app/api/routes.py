ff67d4524964705d0536fab029bc703d064fc121
Scott Munro <scottnmunro@gmail.com>
2017-03-27 13:23:52 -0500

    api endpoints return loops of each list (up to a maxsize)

--- app/api/routes.py
+++ app/api/routes.py
@@ -6,6 +6,11 @@ from app.api import food_data
 
 API_BP = flask.Blueprint('api', __name__)
 
+
+################
+# Browse Views #
+################
+
 """
 This temporary mock variable exists to simulate that we have 50 items
 for each entry. In reality we will just simulate having 50 items for
@@ -13,42 +18,70 @@ each by looping the lists over and over
 """
 MOCK_DATA_MAX_SIZE = 50
 
-################
-# Browse Views #
-################
+"""
+This function will make a mock list from looping a source list up to a maxsize
+
+PARAMS:
+li       a list to loop
+page     the row of results to return
+pagesize the size of the row to return
+maxsize  the size of the mocked out loop list
+"""
+def mock_loop_list(li, page, pagesize, maxsize):
+    # pylint: disable=invalid-name
+    assert page >= 0
+    assert pagesize > 0
+    assert maxsize > 0
+    assert page * pagesize < maxsize
+
+    first_entry = (page * pagesize) % len(li)
+    resultlist = li[first_entry : min(first_entry + pagesize, len(li))]
+    entries_left = max(0, pagesize - len(resultlist))
+    while entries_left > 0:
+        entries_to_add = min(len(li), entries_left)
+        resultlist += li[:entries_to_add]
+        entries_left = entries_left - entries_to_add
+
+    assert len(resultlist) > 0
+    return resultlist
 
 def continuation_route(route_fn):
-    from flask import request
+    from flask import request as req
     @wraps(route_fn)
     def wrapped_route_function(*args, **kwargs):
-        page = request.args.get("page") if "page" in request.args else 0
-        psize = request.args.get("page_size") if "page_size" in request.args else 0
+        page = int(req.args.get("page")) if "page" in req.args else 0
+        psize = int(req.args.get("pagesize")) if "pagesize" in req.args else 10
         return route_fn(page, psize, *args, **kwargs)
     return wrapped_route_function
 
 
 @API_BP.route('/ingredients')
 @continuation_route
-def get_all_ingredients(page, page_size):
-    return flask.json.jsonify({"page": page, "page_size": page_size,
-                               "type": "ingredients"})
+def get_all_ingredients(page: int, pagesize: int):
+    mock_data = mock_loop_list(food_data.ingredients, page, pagesize,
+                               MOCK_DATA_MAX_SIZE)
+    return flask.json.jsonify({"data": mock_data})
+
 @API_BP.route('/recipes')
 @continuation_route
-def get_all_recipes(page, page_size):
-    return flask.json.jsonify({"page": page, "page_size": page_size,
-                               "type": "recipes"})
+def get_all_recipes(page: int, pagesize: int):
+    mock_data = mock_loop_list(food_data.recipes, page, pagesize,
+                               MOCK_DATA_MAX_SIZE)
+    return flask.json.jsonify({"data": mock_data})
 
 @API_BP.route('/grocery_items/')
 @continuation_route
-def get_all_grocery_items(page, page_size):
-    return flask.json.jsonify({"page": page, "page_size": page_size,
-                               "type": "grocery items"})
+def get_all_grocery_items(page: int, pagesize: int):
+    mock_data = mock_loop_list(food_data.grocery_items, page, pagesize,
+                               MOCK_DATA_MAX_SIZE)
+    return flask.json.jsonify({"data": mock_data})
 
 @API_BP.route('/tags')
 @continuation_route
-def get_all_tags(page, page_size):
-    return flask.json.jsonify({"page": page, "page_size": page_size,
-                               "type": "tags"})
+def get_all_tags(page: int, pagesize: int):
+    mock_data = mock_loop_list(food_data.tags, page, pagesize,
+                               MOCK_DATA_MAX_SIZE)
+    return flask.json.jsonify({"data": mock_data})
 
 
 ################
