60923f3d458369b7b9a6701c5765f80279aaf445
Scott Hornberger <scott.hornberger@utexas.edu>
2017-03-27 15:47:15 -0500

    implement all grid modules on grid pages.

--- vf-react/src/js/pages/Ingredients.js
+++ vf-react/src/js/pages/Ingredients.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { IndexLink, Link } from "react-router";
 
+import IngredientItem from "../components/layout/IngredientItem";
+import Greeting from "../components/layout/Greeting";
+
 var data = require('json!../../data/food.json');
+const ingredients = data.ingredients;
+
 
 export default class Ingredients extends React.Component {
+  getGridItems() {
+    var i=0;
+    const gridItems=[];
+    for (var id in ingredients) {
+      gridItems[i++] = <IngredientItem key={id} ingredient={ingredients[id]} />
+    }
+    return gridItems;
+  }
 
   render() {
-    const ingredients = data.ingredients;
-
     return (
       <div id="unique-content">
-          <div class="container">
-            <div class="col-sm-8 col-sm-offset-2 text-center">
-              <h4>Nom nom nom...</h4>
-              <img class="venn" src="../static/images/diagram.png" />
-              <p>Let us help you help yourself to some tasty food.</p>
-            </div>
-          </div>
+          <Greeting />
 
           <div class="container">
             <div class="col-lg-1 dropdown">
@@ -46,122 +51,7 @@ export default class Ingredients extends React.Component {
           </div>
 
           <div id="grid-results" class="row">
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-                <div class="image">
-                  <img class="img img-responsive full-width" src={ingredients[1].image} />
-                </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[1].name}</h3>
-                    <p>{ingredients[1].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/1">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[2].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[2].name}</h3>
-                    <p>{ingredients[2].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/2">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[3].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[3].name}</h3>
-                    <p>{ingredients[3].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/3">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[4].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[4].name}</h3>
-                    <p>{ingredients[4].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/4">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[5].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[5].name}</h3>
-                    <p>{ingredients[5].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/5">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[6].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[6].name}</h3>
-                    <p>{ingredients[6].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/6">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[7].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[7].name}</h3>
-                    <p>{ingredients[7].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/7">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[8].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[8].name}</h3>
-                    <p>{ingredients[8].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/8">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 col-xs-12 col-md-4">
-              <div class="thumbnail">
-              <div class="image">
-                <img class="img img-responsive full-width" src={ingredients[9].image} />
-              </div>
-                <div class="caption">
-                    <h3 class="grid">{ingredients[9].name}</h3>
-                    <p>{ingredients[9].blurb}</p>
-                    <p><Link class="btn btn-primary" role="button" to="ingredients/9">Details</Link></p>
-                </div>
-              </div>
-            </div>
+            {this.getGridItems()}
 
           </div>
       </div>
--- vf-react/src/js/pages/Tags.js
+++ vf-react/src/js/pages/Tags.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { IndexLink, Link } from "react-router";
 
-var data = require('json!../../data/food.json');
+import TagItem from "../components/layout/TagItem";
+import Greeting from "../components/layout/Greeting";
+
+const data = require('json!../../data/food.json');
+const tags = data.tags;
 
 export default class Tags extends React.Component {
-  getName(name) {
-    return "http://placehold.it/500?text=" + name;
+  getGridItems() {
+    console.log(tags);
+    var i=0;
+    const gridItems=[];
+    for (var id in tags) {
+      gridItems[i++] = <TagItem key={id} tag={tags[id]} />
+    }
+    return gridItems;
   }
   render() {
-    const tags = data["tags"];
-
-
     return (
       <div id="unique-content">
-          <div class="container">
-            <div class="col-sm-8 col-sm-offset-2 text-center">
-              <h4>Nom nom nom...</h4>
-              <img class="venn" src="../static/images/diagram.png" />
-              <p>Let us help you help yourself to some tasty food.</p>
-            </div>
-          </div>
+        <Greeting />
 
           <div class="container">
             <div class="offset-1 col-lg-1 dropdown">
@@ -50,45 +51,7 @@ export default class Tags extends React.Component {
 
           <div id="grid-results" class="row">
 
-            <div class="col-sm-6 col-md-4">
-              <div class="thumbnail">
-                <img src={tags[1].image} />
-                <div class="caption">
-                    <h3 class="grid">{tags[1].name}</h3>
-                    <p><Link class="btn btn-primary" role="button" to="tags/1">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-sm-6 col-md-4">
-              <div class="thumbnail">
-                <img src={tags[2].image} />
-                <div class="caption">
-                    <h3 class="grid">{tags[2].name}</h3>
-                    <p><Link class="btn btn-primary" role="button" to="tags/2">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-sm-6 col-md-4">
-              <div class="thumbnail">
-                <img src={tags[3].image} />
-                <div class="caption">
-                    <h3 class="grid">{tags[3].name}</h3>
-                    <p><Link class="btn btn-primary" role="button" to="tags/3">Details</Link></p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-sm-6 col-md-4">
-              <div class="thumbnail">
-                <img src={tags[4].image} />
-                <div class="caption">
-                    <h3 class="grid">{tags[4].name}</h3>
-                    <p><Link class="btn btn-primary" role="button" to="tags/4">Details</Link></p>
-                </div>
-              </div>
-            </div>
+            {this.getGridItems()}
 
           </div>
       </div>
