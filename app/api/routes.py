e417580385b9b14d53024c71588ec26a3e36d9c7
Scott Munro <scottnmunro@gmail.com>
2017-03-29 21:23:03 -0500

    added support for sorting and filtering
    also updated pagesize param to be page_size

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-docstring
+# pylint: disable=invalid-sequence-index
 
 
 from functools import wraps
@@ -7,7 +8,7 @@ import math
 import flask
 
 from app.api import food_data
-from typing import Any, List, Callable
+from typing import Any, Callable, List, Tuple
 
 API_BP = flask.Blueprint('api', __name__)
 
@@ -29,21 +30,21 @@ This function will make a mock list from looping a source list up to a maxsize
 PARAMS:
 li       a list to loop
 page     the row of results to return
-pagesize the size of the row to return
+page_size the size of the row to return
 maxsize  the size of the mocked out loop list
 """
 
 
-def mock_loop_list(li: List[Any], page: int, pagesize: int, maxsize: int):
+def mock_loop_list(li: List[Any], page: int, page_size: int, maxsize: int):
     # pylint: disable=invalid-name
     assert page >= 0
-    assert pagesize > 0
+    assert page_size > 0
     assert maxsize > 0
-    assert page * pagesize < maxsize
+    assert page * page_size < maxsize
 
-    first_entry = (page * pagesize) % len(li)
-    resultlist = li[first_entry: min(first_entry + pagesize, len(li))]
-    entries_left = max(0, pagesize - len(resultlist))
+    first_entry = (page * page_size) % len(li)
+    resultlist = li[first_entry: min(first_entry + page_size, len(li))]
+    entries_left = max(0, page_size - len(resultlist))
     while entries_left > 0:
         entries_to_add = min(len(li), entries_left)
         resultlist += li[:entries_to_add]
@@ -52,15 +53,35 @@ def mock_loop_list(li: List[Any], page: int, pagesize: int, maxsize: int):
     assert len(resultlist) > 0
     return resultlist
 
+def loop_filter_sort(page: int, page_size: int, filters: List[int],
+                     sort_key: Tuple[Callable[[Any], bool], bool],
+                     li: List[Any]):
+    # pylint: disable=invalid-name
+    li = sorted(li, key=sort_key[0], reverse=sort_key[1])
+    li = mock_loop_list(li, page, page_size, MOCK_DATA_MAX_SIZE)
+    return list(filter(tag_filter(filters), li))
+
+
+def tag_filter(tag_filters: List[int]):
+    def filter_func(ele: Any):
+        if len(tag_filters) == 0:
+            return True
+
+        for tag in tag_filters:
+            if tag in ele["tags"]:
+                return True
+        return False
+    return filter_func
+
 
 def get_continuation_links(base_url: str, page: int, psize: int, maxsize: int):
     total_pages = int(math.ceil(maxsize / psize))
     last_page = total_pages - 1
-    url_template = base_url + "?page={page}&pagesize={pagesize}"
-    first_link = url_template.format(page=0, pagesize=psize)
-    prev_link = url_template.format(page=(page - 1), pagesize=psize)
-    next_link = url_template.format(page=(page + 1), pagesize=psize)
-    last_link = url_template.format(page=last_page, pagesize=psize)
+    url_template = base_url + "?page={page}&page_size={page_size}"
+    first_link = url_template.format(page=0, page_size=psize)
+    prev_link = url_template.format(page=(page - 1), page_size=psize)
+    next_link = url_template.format(page=(page + 1), page_size=psize)
+    last_link = url_template.format(page=last_page, page_size=psize)
 
     # first page
     if page == 0:
@@ -83,51 +104,68 @@ def get_continuation_links(base_url: str, page: int, psize: int, maxsize: int):
 def continuation_route(route_fn: Callable[[int, int], Callable]):
     from flask import request as req
 
+    sort_functions = {
+        "alpha": (lambda ele: ele["name"], False),
+        "alpha_reverse": (lambda ele: ele["name"], True),
+        "ready_time_asc": (lambda ele: ele["ready_time"], False),
+        "ready_time_desc": (lambda ele: ele["ready_time"], True),
+        "unsorted": (lambda ele: 0, False)
+    }
+
     @wraps(route_fn)
     def wrapped_route_function(*args, **kwargs):
         page = int(req.args.get("page")) if "page" in req.args else 0
-        psize = int(req.args.get("pagesize")) if "pagesize" in req.args else 10
+        psize = int(req.args.get("page_size")) if "page_size" in req.args else 10
         if page * psize >= MOCK_DATA_MAX_SIZE:
             flask.abort(404)
         else:
             links = get_continuation_links(req.base_url, page, psize,
                                            MOCK_DATA_MAX_SIZE)
+            sort_arg = req.args.get("sort") if "sort" in req.args else "unsorted"
+            sorter = sort_functions[sort_arg]
+            tag_args = req.args.get("tags").split(",") if "tags" in req.args \
+                                                       else []
+            taglist = [int(tag) for tag in tag_args]
             data = flask.json.loads(
-                route_fn(page, psize, *args, **kwargs).data)
+                route_fn(page, psize, taglist, sorter, *args, **kwargs).data)
             return flask.json.jsonify({"data": data, "links": links})
     return wrapped_route_function
 
 
 @API_BP.route('/ingredients')
 @continuation_route
-def get_all_ingredients(page: int, pagesize: int):
-    mock_data = mock_loop_list(food_data.ingredients, page, pagesize,
-                               MOCK_DATA_MAX_SIZE)
-    return flask.json.jsonify({"data": mock_data})
+def get_all_ingredients(page: int, page_size: int, filters: List[int],
+                        sort_key: Tuple[Callable[[Any], bool], bool]):
+    mock_data = loop_filter_sort(page, page_size, filters, sort_key,
+                                 food_data.ingredients)
+    return flask.json.jsonify(mock_data)
 
 
 @API_BP.route('/recipes')
 @continuation_route
-def get_all_recipes(page: int, pagesize: int):
-    mock_data = mock_loop_list(food_data.recipes, page, pagesize,
-                               MOCK_DATA_MAX_SIZE)
-    return flask.json.jsonify({"data": mock_data})
+def get_all_recipes(page: int, page_size: int, filters: List[int],
+                    sort_key: Tuple[Callable[[Any], bool], bool]):
+    mock_data = loop_filter_sort(page, page_size, filters, sort_key,
+                                 food_data.recipes)
+    return flask.json.jsonify(mock_data)
 
 
 @API_BP.route('/grocery_items/')
 @continuation_route
-def get_all_grocery_items(page: int, pagesize: int):
-    mock_data = mock_loop_list(food_data.grocery_items, page, pagesize,
-                               MOCK_DATA_MAX_SIZE)
-    return flask.json.jsonify({"data": mock_data})
+def get_all_grocery_items(page: int, page_size: int, filters: List[int],
+                          sort_key: Tuple[Callable[[Any], bool], bool]):
+    mock_data = loop_filter_sort(page, page_size, filters, sort_key,
+                                 food_data.grocery_items)
+    return flask.json.jsonify(mock_data)
 
 
 @API_BP.route('/tags')
 @continuation_route
-def get_all_tags(page: int, pagesize: int):
-    mock_data = mock_loop_list(food_data.tags, page, pagesize,
-                               MOCK_DATA_MAX_SIZE)
-    return flask.json.jsonify({"data": mock_data})
+def get_all_tags(page: int, page_size: int, filters: List[int],
+                 sort_key: Tuple[Callable[[Any], bool], bool]):
+    mock_data = loop_filter_sort(page, page_size, filters, sort_key,
+                                 food_data.grocery_items)
+    return flask.json.jsonify(mock_data)
 
 
 ################
