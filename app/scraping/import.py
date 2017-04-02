ab4cb4b5f8a73fd3f3e0b58eeb4a0e1a6e0d0586
Noel Benzinger <noel.benzinger@utexas.edu>
2017-04-02 15:29:18 -0500

    Setup for importing.

--- app/scraping/import.py
+++ app/scraping/import.py
@@ -1,4 +1,35 @@
 
-# Import from json into database.
+"""
+Imports json data into the database.
+"""
+
+from pathlib import Path
+from app.api import models
+
+class Import:
+"""
+Imports json data from a directory into a databse.
+"""
+
+    def __init__(self, data_dir:str, db):
+        """
+        data_dir - a directory containing the necessary json files.
+        db       - a SQLAlchemy database instance.
+        """
+
+        self.db = db
+        self.data_dir = Path(data_dir)
+
+    def run():
+        """
+        Run the importation process.
+        """
+
+        models.Ingredient(8, "star anise", "somesite.com/image/star_anise.jpg")
+
+
+if __name__ == "__main__":
+    imp = Import("data", models.db)
+    imp.run()
 
 
