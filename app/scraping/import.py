20e1ef6a8a0d01aea39897d0a21ec010e00edfe7
Noel Benzinger <noel.benzinger@utexas.edu>
2017-04-02 15:53:35 -0500

    Simple write and read to database.

--- app/scraping/import.py
+++ app/scraping/import.py
@@ -4,12 +4,13 @@ Imports json data into the database.
 """
 
 from pathlib import Path
+from flask import Flask
 from app.api import models
 
 class Import:
-"""
-Imports json data from a directory into a databse.
-"""
+    """
+    Imports json data from a directory into a databse.
+    """
 
     def __init__(self, data_dir:str, db):
         """
@@ -20,16 +21,41 @@ Imports json data from a directory into a databse.
         self.db = db
         self.data_dir = Path(data_dir)
 
-    def run():
+    def run(self):
         """
         Run the importation process.
         """
 
-        models.Ingredient(8, "star anise", "somesite.com/image/star_anise.jpg")
+        ingredient = models.Ingredient(8, "star anise", "someimage.jpg")
+        self.db.session.add(ingredient)
+        self.db.session.commit()
+
+    # TODO: Remove this and add proper unit tests.
+    def check(self):
+        """
+        Make some basic data integrity checks.
+        """
+
+        query = self.db.session.query(models.Ingredient).filter_by(name="star anise")
+        ingredient = query.first()
+
+        assert ingredient.ingredient_id == 8
 
 
 if __name__ == "__main__":
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///:memory:'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = False
+    models.db.init_app(app)
+    ctx = app.app_context()
+    ctx.push()
+    models.db.create_all()
+
     imp = Import("data", models.db)
     imp.run()
+    imp.check()
+
+    ctx.pop()
 
 
