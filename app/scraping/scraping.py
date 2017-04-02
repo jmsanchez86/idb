7ead39afc1325e53da431b6588026e41e7db7a88
Noel Benzinger <noel.benzinger@utexas.edu>
2017-04-01 20:21:29 -0500

    Successfuly scrape with no loops.

--- app/scraping/scraping.py
+++ app/scraping/scraping.py
@@ -1,130 +1,13 @@
 
-
 """
-<Get Random Recipe>
-{
-    id
-    title
-    image (download)
-    instructions
-    readyInMinutes
-
-    tags: [vegeterian, ketogenic, veryPopular, dairyFree, vegan,
-           flutenFree, cheap, sustainable, veryHealth, lowFoodmap]
-          <Classify Cuisine>: [african, chinese, japanese, korean,
-                               vietnamese, thai, indian, british, irish,
-                               french, italian, mexican, spanish,
-                               middleEastern, jewish, american, cajun,
-                               southern, greek, german, nordic,
-                               easternEuropean, carribean, latinAmerican]
-
-    description: <Summarize Recipe>
-    ingredients: [
-        id
-        amount
-        unit
-        originalString
-    ]
-}
-
-<Find Similar Recipes>
-id = recipe id
-[
-    {
-        id
-        title
-        readyInMinutes
-    }
-]
-
-<Get Food Information>
-{
-    image
-}
-
-<Get Ingredient Substitutes by Id>
-{
-    status
-    substitutes: [
-        substitute:string,
-        ...
-    ]
-}
-
-<Map Ingredients to Grocery Products>
-[
-    {
-        id
-        upc
-        title
-    }
-]
-
-<Get Product Information>
-{
-    images: [something, somethingElse, somethingElseElse]
-    badges: ["stuff", "stuff2", ...]
-}
-"""
-
-
-"""
-start:
-    <Get Random Recipe>
-    limitLicense = false
-    number = 1
-    tags = ""
-    # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1
-    <Classify Cuisine>
-        {
-            "ingredientList" = newline concatanation of ingredients originalString
-            "title" = recipe title
-        }
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/cuisine
-    <Summarize Recipe>
-        id = recipeId
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/{id}/summary
-    <Find Similar Recipes>
-        id = recipeId
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/{id}/similar
-    for each ingredientId
-        call ingredient(ingredientId)
-
-
-ingredient:
-    <Get Food Information>
-        id = ingredientId
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/{id}/information
-
-    <Get Ingredient Substitute by Id>
-        id = ingredientId
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/{id}/substitutes
-    <Map Ingredients to Grocery Products>
-        {
-            "ingredients": [ingredientName],
-            "servings": 1,
-        }
-        # POST https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/map
-    for each groceryId
-        call product(groceryId)
-
-product:
-    <Get Product Information>
-        id = groceryId
-        # https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/{id}
-
-    call tag()
-
-tag:
-    Give human names
-    Custom images
-    Custom descriptions
-
-
+Read data from soure API and write json results to files.
 """
 
 import json
+import requests
 from http.client import HTTPSConnection
+from config import api_key
+from functools import reduce
 
 class ConnectionException(Exception):
     """
@@ -137,51 +20,120 @@ class ConnectionException(Exception):
 
 class Connection:
     """
-    Light wrapper over an HTTP connection for making consecutive
-    requests to a restful API.
+    Lightweight class to make requests to a restful API.
     """
 
     def __init__(self, domain_url):
-        self.connection = HTTPSConnection(domain_url)
+        self.domain_url = domain_url
+        self.headers = {
+            "Accept": "application/json",
+            "X-Mashape-Key": api_key}
 
 
-    def request(self, path, post_headers=None):
+    def get_request_json(self, path):
         """
-        Make a request for a resource described by path and returns the
+        Make a get request for a resource described by path and returns the
         response object.
         """
 
-        if post_headers == None:
-            self.connection.request("GET", path)
-            res = self.connection.getresponse()
+        path = self.domain_url + "/" + path
 
-            if res.status != 200:
-                msg = "Request %s returned status %d with reason: %s."
-                msg = msg % (path, res.status, res.reason)
-                raise ConnectionException(self, msg)
+        res = requests.get(path, headers=self.headers)
+        res.raise_for_status()
 
-            return res
+        # if res.status_code != 200:
+        #     msg = "Request %s returned status %d with reason: %s."
+        #     msg = msg % (path, res.status, res.reason)
+        #     raise ConnectionException(self, msg)
 
+        return res.json()
+
+    def post_request_json(self, path, *, post_data=None, json_data=None):
+        """
+        Make a post request for a resource described by path and returns the
+        response object.
+        """
+
+        path = self.domain_url + "/" + path
+
+        res = None
+        if post_data:
+            res = requests.post(path, headers=self.headers, data=post_data)
         else:
+            res = requests.post(path, headers=self.headers, json=json_data)
 
-            self.connection.request("POST", path, post_headers)
-            res = self.connection.getresponse()
+        res.raise_for_status()
 
-            if res.status != 200:
-                msg = "Request %s returned status %d with reason: %s."
-                msg = msg % (path, res.status, res.reason)
-                raise ConnectionException(self, msg)
+        # if res.status != 200:
+        #     msg = "Request %s returned status %d with reason: %s."
+        #     msg = msg % (path, res.status, res.reason)
+        #     raise ConnectionException(self, msg)
 
-            return res
+        return res.json()
+
+
+
+SPOONACULAR_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+
+class SpoonacularConnection(Connection):
+
+    def __init__(self):
+        Connection.__init__(self, SPOONACULAR_URL)
+
+conn = SpoonacularConnection()
+write_queue = {}
+
+def product(grocery_data):
+    grocery_id = grocery_data["id"]
+
+    product_data = conn.get_request_json("food/products/" + str(grocery_id))
+    write_queue["get_product_information/" + str(grocery_id) + ".json"] = product_data
+
+def ingredient(ingredient_data):
+    ingredient_id = ingredient_data["id"]
+    substitute_data = conn.get_request_json("food/ingredients/" + str(ingredient_id) + "/substitutes")
+    write_queue["get_ingredient_substitute/" + str(ingredient_id) + ".json"] = substitute_data
+
+    grocery_list_data = conn.post_request_json("food/ingredients/map", json_data={
+        "ingredients": [ingredient_data["name"]],
+        "servings": 1})
+
+    if len(grocery_list_data) > 0:
+        for grocery_data in grocery_list_data[0]["products"]:
+            product(grocery_data)
+            break
+
+def recipe(recipe_data):
+    recipe_id = recipe_data["id"]
+
+    write_queue["get_random_recipes/" + str(recipe_id) + ".json"] = recipe_data
+    ingredient_names = [ingredient["name"] for ingredient in recipe_data["extendedIngredients"]]
+    cuisine_data = conn.post_request_json("recipes/cuisine", post_data={
+        "ingredientList": reduce(lambda a, b: a + "\n" + b, ingredient_names),
+        "title": recipe_data["title"]})
+
+    write_queue["classify_cuisine/" + str(recipe_id) + ".json"] = cuisine_data
+
+    summary_data = conn.get_request_json("recipes/" + str(recipe_id) + "/summary")
+    write_queue["summarize_recipe/" + str(recipe_id) + ".json"] = summary_data
+
+    similar_recipes_data = conn.get_request_json("recipes/" + str(recipe_id) + "/similar")
+    write_queue["find_similar_recipes/" + str(recipe_id) + ".json"] = similar_recipes_data
+
+    for ingredient_data in recipe_data["extendedIngredients"]:
+        ingredient(ingredient_data)
+        break
+
+def start():
+    recipe_list = conn.get_request_json("recipes/random?limitLicense=false&number=1")
+
+    for recipe_data in recipe_list["recipes"]:
+        recipe(recipe_data)
+        # safety first!
+        break
 
 
 if __name__ == "__main__":
-
-    conn = Connection("jsonplaceholder.typicode.com")
-
-    data = conn.request("/posts/1").read()
-
-    print(json.loads(data))
-
+    start()
 
 
