a6b91e50662047fe5a22e01153dc141dcf78d64b
Scott Munro <scottnmunro@gmail.com>
2017-04-01 21:31:49 -0500

    instead of write queue we are actually writing out the json

--- app/scraping/scraping.py
+++ app/scraping/scraping.py
@@ -6,6 +6,7 @@ Read data from soure API and write json results to files.
 """
 
 from functools import reduce
+import json
 import requests
 from config import api_key
 
@@ -42,21 +43,23 @@ def post_request_json(path, *, post_data=None, json_data=None):
 
     return res.json()
 
-
-write_queue = {}
+def write_json(filename: str, data: dict):
+    with open("data/" + filename, 'w') as f:
+        print("writing " + filename + "...")
+        f.write(json.dumps(data))
 
 def product(grocery_data):
     grocery_id = grocery_data["id"]
     product_data = get_request_json("food/products/" + str(grocery_id))
-    write_queue["get_product_information/" +
-                str(grocery_id) + ".json"] = product_data
+    write_json("get_product_information/" +
+               str(grocery_id) + ".json", product_data)
 
 def ingredient(ingredient_data):
     ingredient_id = ingredient_data["id"]
     substitute_data = get_request_json("food/ingredients/" +
                                        str(ingredient_id) + "/substitutes")
-    write_queue["get_ingredient_substitute/" +
-                str(ingredient_id) + ".json"] = substitute_data
+    write_json("get_ingredient_substitute/" +
+               str(ingredient_id) + ".json", substitute_data)
 
     grocery_list_data = post_request_json("food/ingredients/map", json_data={
         "ingredients": [ingredient_data["name"]],
@@ -70,21 +73,16 @@ def ingredient(ingredient_data):
 def recipe(recipe_data):
     recipe_id = recipe_data["id"]
 
-    write_queue["get_random_recipes/" + str(recipe_id) + ".json"] = recipe_data
+    write_json("get_random_recipes/" + str(recipe_id) + ".json", recipe_data)
     ingredient_names = [i["name"] for i in recipe_data["extendedIngredients"]]
-    cuisine_data = post_request_json("recipes/cuisine", post_data={
-        "ingredientList": reduce(lambda a, b: a + "\n" + b, ingredient_names),
-        "title": recipe_data["title"]})
-
-    write_queue["classify_cuisine/" + str(recipe_id) + ".json"] = cuisine_data
 
     summary_data = get_request_json("recipes/" + str(recipe_id) + "/summary")
-    write_queue["summarize_recipe/" + str(recipe_id) + ".json"] = summary_data
+    write_json("summarize_recipe/" + str(recipe_id) + ".json", summary_data)
 
     similar_recipes_data = get_request_json("recipes/" + str(recipe_id) +
                                             "/similar")
-    write_queue["find_similar_recipes/" + str(recipe_id) +
-                ".json"] = similar_recipes_data
+    write_json("find_similar_recipes/" + str(recipe_id) +
+               ".json", similar_recipes_data)
 
     for ingredient_data in recipe_data["extendedIngredients"]:
         ingredient(ingredient_data)
