6f6d69c8d4dff123405e343f7e87b346e54edb86
Noel Benzinger <noel.benzinger@utexas.edu>
2017-04-03 16:12:47 -0500

    Added tag translations.

--- app/scraping/tag_translations.py
+++ app/scraping/tag_translations.py
@@ -5,316 +5,316 @@ tag_descriptions = dict()
 
 tag_names["main course"] = "Main course"
 tag_image_urls["main course"] = ""
-tag_descriptions["main course"] = ""
+tag_descriptions["main course"] = "Dish for the most substantial portion of a meal."
 
 tag_names["free_range"] = "Free range"
 tag_image_urls["free_range"] = ""
-tag_descriptions["free_range"] = ""
+tag_descriptions["free_range"] = "Product animals who were allowed to roam about an open field for a portion of the day."
 
 tag_names["snack"] = "Snack"
 tag_image_urls["snack"] = ""
-tag_descriptions["snack"] = ""
+tag_descriptions["snack"] = "A small amount of food to refuel the mind and remain focused."
 
 tag_names["veryHealthy"] = "Very healthy"
 tag_image_urls["veryHealthy"] = ""
-tag_descriptions["veryHealthy"] = ""
+tag_descriptions["veryHealthy"] = "A abundantly healthy option for the body."
 
 tag_names["pescetarian"] = "Pescetarian"
 tag_image_urls["pescetarian"] = ""
-tag_descriptions["pescetarian"] = ""
+tag_descriptions["pescetarian"] = "Diet based on seafood."
 
 tag_names["dairy_free"] = "Dairy-free"
 tag_image_urls["dairy_free"] = ""
-tag_descriptions["dairy_free"] = ""
+tag_descriptions["dairy_free"] = "Contains no dairy products."
 
 tag_names["latin american"] = "Latin American"
 tag_image_urls["latin american"] = ""
-tag_descriptions["latin american"] = ""
+tag_descriptions["latin american"] = "Dish from or based on Latin American culture."
 
 tag_names["south american"] = "South American"
 tag_image_urls["south american"] = ""
-tag_descriptions["south american"] = ""
+tag_descriptions["south american"] = "Dish from or based on South American culture."
 
 tag_names["french"] = "French"
 tag_image_urls["french"] = ""
-tag_descriptions["french"] = ""
+tag_descriptions["french"] = "Dish from or based on French culture."
 
 tag_names["morning meal"] = "Morning meal"
 tag_image_urls["morning meal"] = ""
-tag_descriptions["morning meal"] = ""
+tag_descriptions["morning meal"] = "Can be consumed in the morning."
 
 tag_names["american"] = "American"
 tag_image_urls["american"] = ""
-tag_descriptions["american"] = ""
+tag_descriptions["american"] = "Dish from or based on American culture."
 
 tag_names["asian"] = "Asian"
 tag_image_urls["asian"] = ""
-tag_descriptions["asian"] = ""
+tag_descriptions["asian"] = "Dish from or based on Asian culture."
 
 tag_names["drink"] = "Drink"
 tag_image_urls["drink"] = ""
-tag_descriptions["drink"] = ""
+tag_descriptions["drink"] = "A beverage meant to satisfy thirst."
 
 tag_names["primal"] = "Primal"
 tag_image_urls["primal"] = ""
-tag_descriptions["primal"] = ""
+tag_descriptions["primal"] = "Food for pets."
 
 tag_names["starter"] = "Starter"
 tag_image_urls["starter"] = ""
-tag_descriptions["starter"] = ""
+tag_descriptions["starter"] = "Food to start off your party or gathering."
 
 tag_names["grass_fed"] = "Grass-fed"
 tag_image_urls["grass_fed"] = ""
-tag_descriptions["grass_fed"] = ""
+tag_descriptions["grass_fed"] = "Product of animals who primarily ate grass."
 
 tag_names["sulfite_free"] = "Sulfite-free"
 tag_image_urls["sulfite_free"] = ""
-tag_descriptions["sulfite_free"] = ""
+tag_descriptions["sulfite_free"] = "Contains no sulfite."
 
 tag_names["southern"] = "Southern"
 tag_image_urls["southern"] = ""
-tag_descriptions["southern"] = ""
+tag_descriptions["southern"] = "Dish from or based on Southern American culture."
 
 tag_names["no_artificial_colors"] = "No artificial colors"
 tag_image_urls["no_artificial_colors"] = ""
-tag_descriptions["no_artificial_colors"] = ""
+tag_descriptions["no_artificial_colors"] = "Contains no artificial food coloring."
 
 tag_names["wild_caught"] = "Wild-caught"
 tag_image_urls["wild_caught"] = ""
-tag_descriptions["wild_caught"] = ""
+tag_descriptions["wild_caught"] = "Product of animals caught in the wild instead of raised on a farm."
 
 tag_names["grain_free"] = "Grain-free"
 tag_image_urls["grain_free"] = ""
-tag_descriptions["grain_free"] = ""
+tag_descriptions["grain_free"] = "Product of animals who primarily ate grains."
 
 tag_names["vegetarian"] = "Vegetarian"
 tag_image_urls["vegetarian"] = ""
-tag_descriptions["vegetarian"] = ""
+tag_descriptions["vegetarian"] = "Food that contains no animal meat."
 
 tag_names["sauce"] = "Sauce"
 tag_image_urls["sauce"] = ""
-tag_descriptions["sauce"] = ""
+tag_descriptions["sauce"] = "A flavoring ingredient of liquid-like consistency."
 
 tag_names["breakfast"] = "Breakfeast"
 tag_image_urls["breakfast"] = ""
-tag_descriptions["breakfast"] = ""
+tag_descriptions["breakfast"] = "Food that goes well in the morning."
 
 tag_names["hor d'oeuvre"] = "Hor d'oeuvre"
 tag_image_urls["hor d'oeuvre"] = ""
-tag_descriptions["hor d'oeuvre"] = ""
+tag_descriptions["hor d'oeuvre"] = "Small portion of food served before a meal."
 
 tag_names["glutenFree"] = "Gluten-free"
 tag_image_urls["glutenFree"] = ""
-tag_descriptions["glutenFree"] = ""
+tag_descriptions["glutenFree"] = "Contains no gluten."
 
 tag_names["condiment"] = "Condiment"
 tag_image_urls["condiment"] = ""
-tag_descriptions["condiment"] = ""
+tag_descriptions["condiment"] = "Items that can be added to food served optionally on the side."
 
 tag_names["indian"] = "Indian"
 tag_image_urls["indian"] = ""
-tag_descriptions["indian"] = ""
+tag_descriptions["indian"] = "Dish from or based on Indian culture."
 
 tag_names["no_additives"] = "No additives"
 tag_image_urls["no_additives"] = ""
-tag_descriptions["no_additives"] = ""
+tag_descriptions["no_additives"] = "Contains no flavor enhancing substances."
 
 tag_names["sugar_free"] = "Sugar-free"
 tag_image_urls["sugar_free"] = ""
-tag_descriptions["sugar_free"] = ""
+tag_descriptions["sugar_free"] = "Contains no sugar."
 
 tag_names["no_artificial_ingredients"] = "No artificial ingredients"
 tag_image_urls["no_artificial_ingredients"] = ""
-tag_descriptions["no_artificial_ingredients"] = ""
+tag_descriptions["no_artificial_ingredients"] = "Contains no artificial ingredients."
 
 tag_names["egg_free"] = "Egg-free"
 tag_image_urls["egg_free"] = ""
-tag_descriptions["egg_free"] = ""
+tag_descriptions["egg_free"] = "Contains no eggs."
 
 tag_names["kosher"] = "Kosher"
 tag_image_urls["kosher"] = ""
-tag_descriptions["kosher"] = ""
+tag_descriptions["kosher"] = "Food that abides by Jewish dietary restrictions."
 
 tag_names["pasture_raised"] = "Pasture-raised"
 tag_image_urls["pasture_raised"] = ""
-tag_descriptions["pasture_raised"] = ""
+tag_descriptions["pasture_raised"] = "Product of animals raised on pastures."
 
 tag_names["irish"] = "Irish"
 tag_image_urls["irish"] = ""
-tag_descriptions["irish"] = ""
+tag_descriptions["irish"] = "Dish from or based on Irish culture."
 
 tag_names["ketogenic"] = "Ketogenic"
 tag_image_urls["ketogenic"] = ""
-tag_descriptions["ketogenic"] = ""
+tag_descriptions["ketogenic"] = "Food good for low-carb high-fat diets."
 
 tag_names["hormone_free"] = "Hormone-free"
 tag_image_urls["hormone_free"] = ""
-tag_descriptions["hormone_free"] = ""
+tag_descriptions["hormone_free"] = "Product of animals without artifical hormonal influence."
 
 tag_names["vegan"] = "Vegan"
 tag_image_urls["vegan"] = ""
-tag_descriptions["vegan"] = ""
+tag_descriptions["vegan"] = "Food that contains no animal products."
 
 tag_names["appetizer"] = "Appetizer"
 tag_image_urls["appetizer"] = ""
-tag_descriptions["appetizer"] = ""
+tag_descriptions["appetizer"] = "Small portion of food before a meal."
 
 tag_names["mexican"] = "Mexican"
 tag_image_urls["mexican"] = ""
-tag_descriptions["mexican"] = ""
+tag_descriptions["mexican"] = "Dish from or based on Mexican culture."
 
 tag_names["middl eastern"] = "Middle eastern"
 tag_image_urls["middl eastern"] = ""
-tag_descriptions["middl eastern"] = ""
+tag_descriptions["middl eastern"] = "Dish from or based on Middle eastern culture."
 
 tag_names["wheat_free"] = "Wheat-free"
 tag_image_urls["wheat_free"] = ""
-tag_descriptions["wheat_free"] = ""
+tag_descriptions["wheat_free"] = "Contains no wheat products."
 
 tag_names["nut_free"] = "Nut-free"
 tag_image_urls["nut_free"] = ""
-tag_descriptions["nut_free"] = ""
+tag_descriptions["nut_free"] = "Contains no nuts."
 
 tag_names["veryPopular"] = "Very popular"
 tag_image_urls["veryPopular"] = ""
-tag_descriptions["veryPopular"] = ""
+tag_descriptions["veryPopular"] = "Very popular choice."
 
 tag_names["salad"] = "Salad"
 tag_image_urls["salad"] = ""
-tag_descriptions["salad"] = ""
+tag_descriptions["salad"] = "A mix of various vegtables normally served with a dressing."
 
 tag_names["msg_free"] = "MSG-free"
 tag_image_urls["msg_free"] = ""
-tag_descriptions["msg_free"] = ""
+tag_descriptions["msg_free"] = "Contains no monosodium glutamate."
 
 tag_names["whole30"] = "Whole30"
 tag_image_urls["whole30"] = ""
-tag_descriptions["whole30"] = ""
+tag_descriptions["whole30"] = "A dietary program aimed at noticably improving people's lifes in only 30 days."
 
 tag_names["antipasti"] = "Antipasti"
 tag_image_urls["antipasti"] = ""
-tag_descriptions["antipasti"] = ""
+tag_descriptions["antipasti"] = "The first course of a traditional Italian meal."
 
 tag_names["soup"] = "Soup"
 tag_image_urls["soup"] = ""
-tag_descriptions["soup"] = ""
+tag_descriptions["soup"] = "A dish that has a primarily liquid consistency."
 
 tag_names["no_preservatives"] = "No preservatives"
 tag_image_urls["no_preservatives"] = ""
-tag_descriptions["no_preservatives"] = ""
+tag_descriptions["no_preservatives"] = "Contains no artificial preservatives."
 
 tag_names["paleo"] = "Paleo"
 tag_image_urls["paleo"] = ""
-tag_descriptions["paleo"] = ""
+tag_descriptions["paleo"] = "A diet consisting of a ballanced assortment of healthy foods."
 
 tag_names["no_artificial_flavors"] = "No artificial flavors"
 tag_image_urls["no_artificial_flavors"] = ""
-tag_descriptions["no_artificial_flavors"] = ""
+tag_descriptions["no_artificial_flavors"] = "Contains no artificial flavoring substances."
 
 tag_names["side dish"] = "Side dish"
 tag_image_urls["side dish"] = ""
-tag_descriptions["side dish"] = ""
+tag_descriptions["side dish"] = "An extra dish consumed along side a main course dish."
 
 tag_names["dip"] = "Dip"
 tag_image_urls["dip"] = ""
-tag_descriptions["dip"] = ""
+tag_descriptions["dip"] = "A sauce for diping other foods in."
 
 tag_names["lowFodmap"] = "Low FODMAP"
 tag_image_urls["lowFodmap"] = ""
-tag_descriptions["lowFodmap"] = ""
+tag_descriptions["lowFodmap"] = "Contains only small amounts of FODMAP compounds."
 
 tag_names["italian"] = "Italian"
 tag_image_urls["italian"] = ""
-tag_descriptions["italian"] = ""
+tag_descriptions["italian"] = "Dish from or based on Italian culture."
 
 tag_names["fingerfood"] = "Finger food"
 tag_image_urls["fingerfood"] = ""
-tag_descriptions["fingerfood"] = ""
+tag_descriptions["fingerfood"] = "Food meant to be consumed by hand without eating utensils."
 
 tag_names["gluten_free"] = "Gluten-free"
 tag_image_urls["gluten_free"] = ""
-tag_descriptions["gluten_free"] = ""
+tag_descriptions["gluten_free"] = "Contains no gluten."
 
 tag_names["peanut_free"] = "Peanut-free"
 tag_image_urls["peanut_free"] = ""
-tag_descriptions["peanut_free"] = ""
+tag_descriptions["peanut_free"] = "Contains no peanuts."
 
 tag_names["brunch"] = "Brunch"
 tag_image_urls["brunch"] = ""
-tag_descriptions["brunch"] = ""
+tag_descriptions["brunch"] = "A large breakfeast like meal consume around lunch time."
 
 tag_names["main dish"] = "Main dish"
 tag_image_urls["main dish"] = ""
-tag_descriptions["main dish"] = ""
+tag_descriptions["main dish"] = "The most substantial dish of a meal."
 
 tag_names["fair_trade"] = "Fair trade"
 tag_image_urls["fair_trade"] = ""
-tag_descriptions["fair_trade"] = ""
+tag_descriptions["fair_trade"] = "Product has a ballanced price between consumers and producers."
 
 tag_names["antipasto"] = "Antipasto"
 tag_image_urls["antipasto"] = ""
-tag_descriptions["antipasto"] = ""
+tag_descriptions["antipasto"] = "The first course of a traditional Italian meal."
 
 tag_names["mediterranean"] = "Mediterranean"
 tag_image_urls["mediterranean"] = ""
-tag_descriptions["mediterranean"] = ""
+tag_descriptions["mediterranean"] = "Dish from or based on Mediterranean culture."
 
 tag_names["beverage"] = "Beverage"
 tag_image_urls["beverage"] = ""
-tag_descriptions["beverage"] = ""
+tag_descriptions["beverage"] = "A drink meant to satisfy thirst."
 
 tag_names["lunch"] = "Lunch"
 tag_image_urls["lunch"] = ""
-tag_descriptions["lunch"] = ""
+tag_descriptions["lunch"] = "A meal consumed during the mid-day hours."
 
 tag_names["spread"] = "Spread"
 tag_image_urls["spread"] = ""
-tag_descriptions["spread"] = ""
+tag_descriptions["spread"] = "Food with cream-like consistency applied to other foods with a knife."
 
 tag_names["european"] = "European"
 tag_image_urls["european"] = ""
-tag_descriptions["european"] = ""
+tag_descriptions["european"] = "Dish from or based on European culture."
 
 tag_names["dessert"] = "Dessert"
 tag_image_urls["dessert"] = ""
-tag_descriptions["dessert"] = ""
+tag_descriptions["dessert"] = "A sweet dish usually served at the end of a meal."
 
 tag_names["organic"] = "Organic"
 tag_image_urls["organic"] = ""
-tag_descriptions["organic"] = ""
+tag_descriptions["organic"] = "Products that do not use hazourdous farming methods such as pesticides."
 
 tag_names["cage_free"] = "Cage-free"
 tag_image_urls["cage_free"] = ""
-tag_descriptions["cage_free"] = ""
+tag_descriptions["cage_free"] = "Product of animals that were not kept in cages."
 
 tag_names["sustainable"] = "Sustainable"
 tag_image_urls["sustainable"] = ""
-tag_descriptions["sustainable"] = ""
+tag_descriptions["sustainable"] = "Food that can support the bodies basic needs."
 
 tag_names["gmo_free"] = "Non-GMO"
 tag_image_urls["gmo_free"] = ""
-tag_descriptions["gmo_free"] = ""
+tag_descriptions["gmo_free"] = "Product that is not genetically modified."
 
 tag_names["dinner"] = "Dinner"
 tag_image_urls["dinner"] = ""
-tag_descriptions["dinner"] = ""
+tag_descriptions["dinner"] = "Meal that can be consumed in the evening."
 
 tag_names["no_added_sugar"] = "No added sugar"
 tag_image_urls["no_added_sugar"] = ""
-tag_descriptions["no_added_sugar"] = ""
+tag_descriptions["no_added_sugar"] = "Contains unnatural sugars."
 
 tag_names["soy_free"] = "Soy-free"
 tag_image_urls["soy_free"] = ""
-tag_descriptions["soy_free"] = ""
+tag_descriptions["soy_free"] = "Contains no soy products."
 
 tag_names["dairyFree"] = "Dairy-free"
 tag_image_urls["dairyFree"] = ""
-tag_descriptions["dairyFree"] = ""
+tag_descriptions["dairyFree"] = "Contains no dairy products."
 
 tag_names["corn_free"] = "Corn-free"
 tag_image_urls["corn_free"] = ""
-tag_descriptions["corn_free"] = ""
+tag_descriptions["corn_free"] = "Contains no corn-based foods."
 
 tag_names["lactose_free"] = "Lactose-free"
 tag_image_urls["lactose_free"] = ""
-tag_descriptions["lactose_free"] = ""
+tag_descriptions["lactose_free"] = "Can be consumed by the lactose-intolerant."
