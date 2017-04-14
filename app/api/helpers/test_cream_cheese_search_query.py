560af69e7591ab4724dde4e0213d50e71cfab4d9
Scott Munro <scottnmunro@gmail.com>
2017-04-13 21:33:10 -0500

    added a list looping function to the test data

--- app/api/helpers/test_cream_cheese_search_query.py
+++ app/api/helpers/test_cream_cheese_search_query.py
@@ -1,12 +1,14 @@
 # pylint: disable=missing-docstring
 
+from typing import Any, List
+
 query_resp =\
     [
         {
-            id: "509488",
-            pillar_name: 'recipes',
-            image: "https://spoonacular.com/recipeImages/A-High-Protein-Snack--Healthy-Almond-Joy-Cheesecake-%7BGluten-Free--Low-Carb-+-High-Protein%7D-509488.jpg",
-            contexts:
+            "id": "509488",
+            "pillar_name": 'recipes',
+            "image": "https://spoonacular.com/recipeImages/A-High-Protein-Snack--Healthy-Almond-Joy-Cheesecake-%7BGluten-Free--Low-Carb-+-High-Protein%7D-509488.jpg",
+            "contexts":
                 [
                     """Healthy Almond Joy "<span class="search-context">Cheese</span>cake" is a Southern recipe that serves 1""",
                     """Cup Low Fat Cottage <span class=\"search-context\">Cheese""",
@@ -15,41 +17,39 @@ query_resp =\
                 ]
         },
         {
-            id: "408444",
-            pillar_name: "grocery_items",
-            image: "https://spoonacular.com/productImages/408444-636x393.jpg",
-            contexts:
+            "id": "408444",
+            "pillar_name": "grocery_items",
+            "image": "https://spoonacular.com/productImages/408444-636x393.jpg",
+            "contexts":
                 [
-                    """All Natural Sour <span class="search-context">Cream</span>cake"""",
+                    """All Natural Sour <span class="search-context">Cream</span>cake""",
                     """Dairy Sour <span class=\"search-context\">Cream</span> Pure And Natural""",
                     """Shurfresh Sour <span class="search-context">Cream</span> - Cultured Grade A Pasteurized""",
                     """Daisy Brand Sour <span class="search-context">Cream</span>, 5 lb"""
                 ]
         },
         {
-            id: "19228",
-            pillar_name: "ingredients",
-            image: "https://storage.googleapis.com/vennfridge/saved_ingredient_images%2F19228.jpg",
-            contexts:
+            "id": "19228",
+            "pillar_name": "ingredients",
+            "image": "https://storage.googleapis.com/vennfridge/saved_ingredient_images%2F19228.jpg",
+            "contexts":
                 [
-                    """<span class="search-context">Cream Cheese Frosting</span>"""",
-                    """Carrot Sheet Cake with <span class="search-context">Cream Cheese Frosting</span>""""
+                    """<span class="search-context">Cream Cheese Frosting</span>""",
+                    """Carrot Sheet Cake with <span class="search-context">Cream Cheese Frosting</span>"""
                 ]
         },
         {
-            id: "Dip",
-            pillar_name: "tags",
-            image: "http://vennfridge.me/static/images/tags/Dip.png",
-            contexts:
+            "id": "Dip",
+            "pillar_name": "tags",
+            "image": "http://vennfridge.me/static/images/tags/Dip.png",
+            "contexts":
                 [
-                    """Avocado Goat <span class="search-context">Cheese</span> Dip with Whole-Wheat Pita Chips"""",
-                    """Nacho <span class="search-context">Cheese</span> Sauce""""
+                    """Avocado Goat <span class="search-context">Cheese</span> Dip with Whole-Wheat Pita Chips""",
+                    """Nacho <span class="search-context">Cheese</span> Sauce"""
                 ]
         }
     ]
 
-
-"""
 
 def mock_loop_list(li: List[Any], page: int, page_size: int, maxsize: int):
     # pylint: disable=invalid-name
@@ -70,24 +70,5 @@ def mock_loop_list(li: List[Any], page: int, page_size: int, maxsize: int):
     return resultlist
 
 
-def loop_filter_sort(query_params: QueryParams, li: List[Any]):
-    # pylint: disable=invalid-name
-
-    def generate_tag_filter(tag_filters: List[int]):
-        def filter_func(ele: Any):
-            if len(tag_filters) == 0:
-                return True
-
-            for tag in tag_filters:
-                if tag in ele["tags"]:
-                    return True
-            return False
-        return filter_func
-
-    li = sorted(li, key=query_params.sort_key[0],
-                reverse=query_params.sort_key[1])
-    li = mock_loop_list(li, query_params.page, query_params.page_size,
-                        MOCK_DATA_MAX_SIZE)
-    return list(filter(generate_tag_filter(query_params.tag_filters), li))
-
-"""
+def get_test_search_query(page: int, page_size: int, maxsize: int):
+    return mock_loop_list(query_resp, page, page_size, maxsize)
