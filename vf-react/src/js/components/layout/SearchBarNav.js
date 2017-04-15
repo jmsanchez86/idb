2048d035c975674c9e39808ab0b34bf6bfd0cfea
Scott Hornberger <scott.hornberger@utexas.edu>
2017-04-15 17:44:17 -0500

    Code styling

--- vf-react/src/js/components/layout/SearchBarNav.js
+++ vf-react/src/js/components/layout/SearchBarNav.js
@@ -29,28 +29,26 @@ export default class SearchBar extends React.Component {
   render() {
     return (
       <span id="NavSearch" class="form-group form-group-md col-lg-3 col-md-3 col-sm-6 pull-right">
-
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <div class="input-group">
-            <input
-              class="search-query form-control"
-              id="TextBox"
-              placeholder="Search our site..."
-              value={this.state.value}
-              onChange={this.handleChange.bind(this)}
-              />
-              <span class="input-group-btn">
-                <button
-                  class="btn btn-md btn-default"
-                  id="SearchButton"
-                  type="submit button"
-                  value="Submit">
-                  <span class="glyphicon glyphicon-search"></span>
-                </button>
-              </span>
-              </div>
-            </form>
-
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div class="input-group">
+          <input
+            class="search-query form-control"
+            id="TextBox"
+            placeholder="Search our site..."
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            />
+            <span class="input-group-btn">
+              <button
+                class="btn btn-md btn-default"
+                id="SearchButton"
+                type="submit button"
+                value="Submit">
+                <span class="glyphicon glyphicon-search"></span>
+              </button>
+            </span>
+          </div>
+        </form>
       </span>
     )
   }
