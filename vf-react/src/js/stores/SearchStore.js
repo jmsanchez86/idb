6d732d03a37139ba8ed907930f14dbfa11667d5d
Scott Hornberger <scott.hornberger@utexas.edu>
2017-04-15 20:06:45 -0500

    Implement Request, Response, and Error handling

--- vf-react/src/js/stores/SearchStore.js
+++ vf-react/src/js/stores/SearchStore.js
@@ -9,52 +9,34 @@ class SearchStore extends EventEmitter {
     }
   }
 
+  handleRequest(obj) {
+    console.log(obj);
+  }
+  handleResponse(obj) {
+    console.log(obj);
+  }
+  handleError(obj) {
+    console.log(obj);
+  }
+
   handleAction(action) {
     switch(action.type) {
-      case "SEARCH": {
-        this.searchValue(action.value);
+      case "SEARCH_REQUEST": {
+        this.handleRequest(action.value);
+        break;
+      }
+      case "SEARCH_RESPONSE": {
+        this.handleResponse(action.obj);
+        break;
+      }
+      case "SEARCH_ERROR": {
+        this.handleError(action.err);
+        break;
       }
     }
   }
-
-  searchValue(value) {
-    console.log(value);
-    var _this = this;
-    var _data = {};
-    var _links = {};
-
-    // call api with new query params
-    fetch("http://api.vennfridge.appspot.com/search?q=test")
-      .then(function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem loading vennfridge info. Status Code: ' +
-              response.status);
-        }
-        response.json().then(function(responseData) {
-          for (var context in responseData.data){
-            _data[context] = responseData.data[context];
-          }
-          for (var elem in responseData.links){
-            _links[elem] = responseData.links[elem];
-          }
-          console.log(_data);
-          console.log(_links);
-          // _this.state.data = _data;
-          // _this.state.links = _links;
-          // _this.forceUpdate();
-
-        });
-      })
-    .catch(function(err) {
-        console.log('Fetch Error: -S', err);
-      });
-  }
-
-
-
 }
 
 const searchStore = new SearchStore;
 Dispatcher.register(searchStore.handleAction.bind(searchStore));
-window.Dispatcher = Dispatcher;
 export default searchStore;
