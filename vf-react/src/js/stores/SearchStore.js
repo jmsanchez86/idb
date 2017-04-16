fac9c7f67f3396b840f6df584d9d1f22527d3a84
Scott Hornberger <scott.hornberger@utexas.edu>
2017-04-15 19:36:15 -0500

    Implement query on SEARCH action

--- vf-react/src/js/stores/SearchStore.js
+++ vf-react/src/js/stores/SearchStore.js
@@ -12,10 +12,44 @@ class SearchStore extends EventEmitter {
   handleAction(action) {
     switch(action.type) {
       case "SEARCH": {
-        console.log(action.value);
+        this.searchValue(action.value);
       }
     }
   }
+
+  searchValue(value) {
+    console.log(value);
+    var _this = this;
+    var _data = {};
+    var _links = {};
+
+    // call api with new query params
+    fetch("http://api.vennfridge.appspot.com/search?q=test")
+      .then(function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem loading vennfridge info. Status Code: ' +
+              response.status);
+        }
+        response.json().then(function(responseData) {
+          for (var context in responseData.data){
+            _data[context] = responseData.data[context];
+          }
+          for (var elem in responseData.links){
+            _links[elem] = responseData.links[elem];
+          }
+          console.log(_data);
+          console.log(_links);
+          // _this.state.data = _data;
+          // _this.state.links = _links;
+          // _this.forceUpdate();
+
+        });
+      })
+    .catch(function(err) {
+        console.log('Fetch Error: -S', err);
+      });
+  }
+
 
 
 }
