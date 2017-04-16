8a894fe9f91b37a507a11edbb52407167826ab47
Scott Hornberger <scott.hornberger@utexas.edu>
2017-04-15 19:07:50 -0500

    SearchStore listening to dispatcher

--- vf-react/src/js/stores/SearchStore.js
+++ vf-react/src/js/stores/SearchStore.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from "events";
+import Dispatcher from "../Dispatcher";
 
 class SearchStore extends EventEmitter {
   constructor() {
@@ -7,12 +8,19 @@ class SearchStore extends EventEmitter {
       value: "",
     }
   }
-  test() {
-    alert("SearchStore");
+
+  handleAction(action) {
+    switch(action.type) {
+      case "SEARCH": {
+        console.log(action.value);
+      }
+    }
   }
 
 
 }
 
 const searchStore = new SearchStore;
+Dispatcher.register(searchStore.handleAction.bind(searchStore));
+window.Dispatcher = Dispatcher;
 export default searchStore;
