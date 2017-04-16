d99dcb0c2f0b5b28595bfaff0c0e2ef5d43f275f
Scott Hornberger <scott.hornberger@utexas.edu>
2017-04-16 01:21:44 -0500

    Implement load info from query response

--- vf-react/src/js/components/layout/SearchItem.js
+++ vf-react/src/js/components/layout/SearchItem.js
@@ -3,50 +3,35 @@ import { IndexLink, Link } from "react-router";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default class SearchItem extends React.Component {
-  getLink(id) {
-    return this.props.path + "/" + id;
+  getLink(item) {
+    return item.pillar_name + "/" + item.id;
   }
+
   render() {
     const item  = this.props.item;
     const image = item.image;
     const name  = item.name;
-    var blurb = item.blurb;
+    const id = item.id;
 
-    var id = item.id;
-    if (this.props.path == 'tags'){
-      id = name;
-    }
-    if (this.props.path == 'recipes'){
-      blurb = blurb.replace(/<(?:.|\n)*?>/gm, '');
-    }
-    const search_index = blurb.indexOf("recipe");
 
     return (
       <div>
       <ListGroupItem class="row">
-        <Link to={this.getLink(id)}>
+        <Link to={this.getLink(item)}>
         <div class="thumbnail col-md-3 col-sm-6 col-xs-12">
           <div class="image">
               <img class="img img-rounded img-responsive thumb" src={image} />
           </div>
         </div>
+        </Link>
         <div class="caption col-md-9 col-sm-6 col-xs-12">
-
+          <Link to={this.getLink(id)}>
             <h5 id="search_item_name" class="search">
             {name && name.length > 50 ? name.substr(0,100) + "..." : name}
             </h5>
-            <p>
-            {blurb ? 
-                (search_index > 0 && search_index < 500 ?
-                    blurb.substr(0,search_index) : (blurb.substr(0, 330)) + "...") :
-                 ""}
-            {blurb && search_index > 0 ? (<b>recipe</b>) : ""}
-            {blurb && search_index > 0 ? blurb.substring(search_index + 6, 330) + "...": ""}
-            </p>
-
+          </Link>
         </div>
-        
-        </Link>
+
       </ListGroupItem>
       </div>
     );
