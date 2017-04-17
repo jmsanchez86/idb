2be26b213c6a88f8ddf5e0148b5d2a0cc7d081ce
Thomas Cardwell <thomascardwell7@gmail.com>
2017-04-17 05:02:30 -0500

    search populates with mock data

--- vf-react/src/js/components/layout/SearchItem.js
+++ vf-react/src/js/components/layout/SearchItem.js
@@ -1,25 +1,19 @@
-  import React from "react";
+import React from "react";
 import { IndexLink, Link } from "react-router";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default class SearchItem extends React.Component {
   getLink(id) {
-    return this.props.path + "/" + id;
+    return this.props.path;
   }
+
   render() {
     const item  = this.props.item;
     const image = item.image;
     const name  = item.name;
-    var blurb = item.blurb;
-
+    var blurb = item.contexts;
+    
     var id = item.id;
-    if (this.props.path == 'tags'){
-      id = name;
-    }
-    if (this.props.path == 'recipes'){
-      blurb = blurb.replace(/<(?:.|\n)*?>/gm, '');
-    }
-    const search_index = blurb.indexOf("recipe");
 
     return (
       <div>
@@ -32,17 +26,12 @@ export default class SearchItem extends React.Component {
         </div>
         <div class="caption col-md-9 col-sm-6 col-xs-12">
 
-            <h5 id="search_item_name" class="search">
+            <h4 id="search_item_name" class="search">
             {name && name.length > 50 ? name.substr(0,100) + "..." : name}
-            </h5>
-            <p>
-            {blurb ? 
-                (search_index > 0 && search_index < 500 ?
-                    blurb.substr(0,search_index) : (blurb.substr(0, 330)) + "...") :
-                 ""}
-            {blurb && search_index > 0 ? (<b>recipe</b>) : ""}
-            {blurb && search_index > 0 ? blurb.substring(search_index + 6, 330) + "...": ""}
-            </p>
+            </h4>
+            
+              <p dangerouslySetInnerHTML={{__html: blurb}} />
+            
 
         </div>
         
