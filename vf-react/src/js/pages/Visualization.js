1d4bdcd767e14c7a223d0e698ab40ecf7446cbe2
Jose Sanchez <jmsanchez8653@hotmail.com>
2017-04-17 20:26:28 -0500

    sweawakens data retrieved successfully and paritally displayed.

--- vf-react/src/js/pages/Visualization.js
+++ vf-react/src/js/pages/Visualization.js
@@ -2,20 +2,30 @@ import React from "react";
 
 import * as d3 from "d3";
 
+var total = 0;
 export default class Visual extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			width : 500,
-			height : 500,
+			count : 0,
+			width : 1920,
+			height : 1080,
+			end : 0,
 			graph : { 	
-						nodes : [], 
-						links : []
+						nodes : [{id : "Hello", group : 10, film : true},
+								 {id : "World", group : 10, film : false}], 
+						links : [{source : "World", target : "Hello", value : 5}]
 					},
+			final_graph : {
+							 nodes : [],
+							 links : []
+						  },
 		}
 	}
 
-	visualize() {
+	visualize(graph) {
+
+		var radius;
 		var svg = d3.select("svg");
 		
 
@@ -30,16 +40,16 @@ export default class Visual extends React.Component {
 		var link = svg.append("g")
 		    .attr("class", "links")
 		  .selectAll("line")
-		  .data(this.state.graph.links)
+		  .data(graph.links)
 		  .enter().append("line")
 		    .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
 		var node = svg.append("g")
 		    .attr("class", "nodes")
 		  .selectAll("circle")
-		  .data(this.state.graph.nodes)
+		  .data(graph.nodes)
 		  .enter().append("circle")
-		    .attr("r", 5)
+		    .attr("r", function(d) { d.film ? radius=15 : radius=9; return radius;})
 		    .attr("fill", function(d) { return color(d.group); })
 		    .call(d3.drag()
 		        .on("start", dragstarted)
@@ -50,11 +60,11 @@ export default class Visual extends React.Component {
 		    .text(function(d) { return d.id; });
 
 		simulation
-		    .nodes(this.state.graph.nodes)
+		    .nodes(graph.nodes)
 		    .on("tick", ticked);
 
 		simulation.force("link")
-		    .links(this.state.graph.links);
+		    .links(graph.links);
 
 		function ticked() {
 		  link
@@ -86,47 +96,88 @@ export default class Visual extends React.Component {
 		}
 	}
 
+	requestData(requestString, graph, film, group) {
+		var _this = this;
+		var count = _this.state.count++;
+		fetch(requestString)
+		  .then(function(response) {
+		     if (response.status !== 200) {
+		         console.log('Looks like there was a problem loading sweawakens info. Status Code: ' +
+		           response.status);
+		     }
+		     response.json().then(function(responseData) {
+		      //console.log(responseData);
+		    	for (var id in responseData) {
+		    		const item = responseData[id];
+		    		
+		    		console.log({id : item.name, group : group, film : false});
+		    		graph.nodes.push({id : item.name, group : group, film : false});
+
+		    		console.log({source : item.name, target : film, value : 5 });
+		    		graph.links.push({source : item.name, target : film, value : 5 });
+		    	}	
+
+		    	    _this.setState({ final_graph : graph, count: count });
+		    	    console.log(_this.state.final_graph);
+		    	    console.log("Count");
+		    	    console.log(_this.state.count);
+		    	    total++;
+		    	    console.log("Total");
+		    	    console.log(total);
+		    	    if (total === 34) {
+		    	    	var setme = new Set(_this.state.final_graph.nodes);
+		    	    	console.log(setme);
+		    	    	_this.buildGraph(_this.state.final_graph);
+		    	    }
+		    	});
+		     })
+		   .catch(function(err) {
+		      console.log('Fetch Error: -S', err);
+		    });
+	}
+
 	componentDidMount() {
 		var _this = this;
 		var graph = {
 			nodes : [],
 			links : []
 		};
+		var request;
+		// Acquire films
+		for (var i = 1; i < 3; i++){
+			request = "http://www.thesweawakens.me/api/films/?page=";
+			request += i;
+			fetch(request)
+		      .then(function(response) {
+		        if (response.status !== 200) {
+		            console.log('Looks like there was a problem loading sweawakens info. Status Code: ' +
+		              response.status);
+		        }
+		        console.log(request);
+		        response.json().then(function(responseData) {
+		    		for (var id in responseData) {
+		    			const film = responseData[id];
+		    			graph.nodes.push({id : film.title, group : film.episode_no, film : true});
+		    			for (var pid in film.planet_list) {
+		    				const planet = film.planet_list[pid];
+		    				_this.requestData(planet, graph, film.title, film.episode_no);
+		    			}
+		    		}	
 
-		/*var myHeaders = new Headers();
+		    		_this.setState({ final_graph : graph});
 
-		var myInit = { method: 'GET',
-		               headers: myHeaders,
-		               mode: 'cors',
-		               cache: 'default' };*/
-
-		fetch("http://www.thesweawakens.me/api/films")
-	      .then(function(response) {
-	        if (response.status !== 200) {
-	            console.log('Looks like there was a problem loading sweawakens info. Status Code: ' +
-	              response.status);
-	        }
-	        response.json().then(function(responseData) {
-	        	console.log(responseData);
-	    		for (var film in responseData) {
-	    			graph.nodes.push({id : responseData[film].title, group : responseData[film].episode_no});
-	    		}	
-
-	    		_this.setState({
-	    		
-	    			graph : graph
-	    		
-	    		});
-	    		
-	    		_this.visualize();
-
-	        });
-	      })
-	    .catch(function(err) {
-	        console.log('Fetch Error: -S', err);
-	      });
+		        });
+		      })
+		    .catch(function(err) {
+		        console.log('Fetch Error: -S', err);
+		      });
+		}
 	}
-	
+
+	buildGraph(graph) {
+		this.visualize(graph);
+	}
+
 	render() {
 		return (
 			<div class="container">
